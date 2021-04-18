6d2cc64e9eff0b30e11128a494dff4ce796108fa
Eda Isaku <eda.isaku@yahoo.com>
2021-04-18 15:58:15 +0200

    Finished Login Page

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -12,7 +12,7 @@ class SignUpForm extends React.Component{
         email: '',
         password: '',
         confirmPass: '',
-        isSignedUp: false
+        isSignedUp: false,
     }
 
     handleChange = (ev) => {
@@ -52,24 +52,57 @@ class SignUpForm extends React.Component{
         })
     }
 
+    //TODO Errors and Validation
+//     validateData = () => {
+//      const {username, email, password, confirmPass} = this.state
+//      this.validateUsername(username)
+
+//    }
+
+//     validateUsername = (username) => {
+//     const userErrors = []    
+//     if(username.trim().length < 3 && !this.state.errors.includes('Username is too short')){
+//         userErrors.push('Username is too short')
+//         }
+//     if(!(/^([^0-9]*)$/).test(username)){
+//         userErrors.push('Username can not contain Numbers')
+//     }
+//     if(userErrors.length >0){
+//         this.setState ({
+//             errors: this.state.errors.concat(userErrors)
+//         })
+//         return false
+//     } else {
+//         return true
+//     }
+//    }
+
+//    validatePassword = (password, confirmPass) => {
+//        const errors = []
+//        if((!password) && (!confirmPass)){
+//         errors.push('Password is required')
+//        }
+
+//    }
 
     render(){
         const {username, email, password, confirmPass,isSignedUp} = this.state
+        
         return (
-                
                 <Fade left duration={600} distance="50px">
                     <div className="form">
                     {isSignedUp ? (<SignUpSuccess/>) : 
                         (<>
-                        <h1 className="form__title">Welcome!</h1>
-                        <p className="form__subtitle">Please Sign Up to start making our country green!</p>
-                        <Input value={username} handleChange={this.handleChange} type="text" name={'username'} placeholder="Name..."/>
-                        <Input value={email} handleChange={this.handleChange} type="email" name={'email'} placeholder="Email..."/>
-                        <Input value={password} handleChange={this.handleChange} type="password" name={'password'} placeholder="Password..."/>
-                        <Input value={confirmPass} handleChange={this.handleChange} type="password" name={'confirmPass'} placeholder="Confirm Password..."/>
-                        <Input handleSubmit={this.handleSubmit} type="button" className={'form__submit'} value="Sign Up"/>
+                            <h1 className="form__title">Welcome!</h1>
+                            <p className="form__subtitle">Please Sign Up to start making our country green!</p>
+                            
+                            <Input value={username}  handleChange={this.handleChange} type="text" name={'username'} placeholder="Name..."/>
+                            <Input value={email} handleChange={this.handleChange} type="email" name={'email'} placeholder="Email..."/>
+                            <Input value={password} handleChange={this.handleChange} type="password" name={'password'} placeholder="Password..."/>
+                            <Input value={confirmPass} handleChange={this.handleChange} type="password" name={'confirmPass'} placeholder="Confirm Password..."/>
+                            <Input handleSubmit={this.handleSubmit} type="button" className={'form__submit'} value="Sign Up"/>
                             <Link to="/signIn" className="form__link">Already signed up?</Link>
-                            </>)}
+                        </>)}
                     </div>
                 </Fade>
         )
