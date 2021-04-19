669753813cde125525a9434f0c1a8e8923fc1b76
Kridi Ramilli <kridiramill@gmail.com>
2021-04-19 13:17:47 +0200

    To return back in method one of Validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,14 +7,14 @@ import "./Input.css"
 function Input ({type, placeholder, value, className, name, handleChange, handleSubmit,style,validation }){
         
     return(
-            <div className={type=='button' ? '':`form__control ${validation.result}`}>
-                <input style={style} onChange={handleChange} onClick={handleSubmit} name={name} className={type=='button' ? 'form__submit':'form__input'} type={type} placeholder={placeholder} value={value} />
+            <div className={type==='button' ? '':`form__control ${validation.result}`}>
+                <input style={style} onChange={handleChange} onClick={handleSubmit} name={name} className={type==='button' ? 'form__submit':'form__input'} type={type} placeholder={placeholder} value={value} />
                 {
-                type=='button' ? null: ( <>
-                <FaRegTimesCircle className="form__control__icons error__icon"/>
-                <FaCheckCircle className="form__control__icons success__icon"/>
-                <small className="form__control__errorMessage">{`*${validation.message}`}</small>
-                </>)
+                    type==='button' ? null: ( <>
+                    <FaRegTimesCircle className="form__control__icons error__icon"/>
+                    <FaCheckCircle className="form__control__icons success__icon"/>
+                    <small className="form__control__errorMessage">{`*${validation.message}`}</small>
+                    </>)
                 }
             </div>)
         
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -15,7 +15,21 @@ class SignUpForm extends React.Component{
         password: '',
         confirmPass: '',
         isSignedUp: false,
-        errors:[]
+        errors:{
+            username:{
+                result:false,
+                message:''
+            },
+            email: {
+                result:false,
+                message:''
+            },
+            password:{
+                result: false,
+                message: ''
+            }
+        },
+        shouldCheckInput: false
     }
 
     handleChange = (ev) => {
@@ -27,6 +41,9 @@ class SignUpForm extends React.Component{
     
     handleSubmit = () => {
         const{email, password} = this.state
+        this.setState({
+            shouldCheckInput: true
+        })
         this.validateData()
         //#region Make request to Firebase
         auth.createUserWithEmailAndPassword(email, password)
@@ -62,6 +79,8 @@ class SignUpForm extends React.Component{
     //TODO Errors and Validation
     validateData = () => {
      const {username, email, password, confirmPass} = this.state
+     this.validateEmail(email)
+     this.validatePassword(password,confirmPass)
      this.validateUsername(username)
 
    }
@@ -75,15 +94,61 @@ class SignUpForm extends React.Component{
         userErrors.push('Username can not contain numbers!')
     }
     if(userErrors.length >0){
-        this.setState ({
-            errors: this.state.errors.concat(userErrors)
+        this.setState ( (prevState) => ({
+            errors: prevState.errors.concat(userErrors)
         })
+        )
         return false
     } else {
-        this.setState({errors:[]}) //HERE
+       
+       this.removeError('username')
         return true
     }
    }
+
+   validateEmail = (email) => {
+       const emailErrors = []
+       let emailTest= new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+       if(email.trim() < 4){
+            emailErrors.push('Email is too short or empty!')
+       }
+
+       if(!emailTest.test(email)){
+        emailErrors.push('Email is not valid format!')
+       }
+       if(emailErrors.length > 0){
+        this.setState ( (prevState) => ({
+            errors: prevState.errors.concat(emailErrors)
+            }))
+        } else {
+             this.removeError('email')
+             return true
+         }
+   }
+
+   validatePassword = (password, confirmPass) => {
+    const passwordErrors = []
+    if((!password) && (!confirmPass)){
+     passwordErrors.push('Password && Confirm Password is required!')
+    }
+    if(password !== confirmPass) {
+     passwordErrors.push("Your password don't match!")
+    }
+
+    if(passwordErrors.length > 0){
+        this.setState( prevState => {
+          return {
+               errors: prevState.errors.concat(passwordErrors)
+            }
+        }
+        )
+        return false
+    } else {
+        this.removeError('password')
+        return true
+    }
+
+}
 
    checkInput = (input) => {
         const {errors} = this.state;
@@ -100,17 +165,19 @@ class SignUpForm extends React.Component{
         return validation;
    }
 
-//    validatePassword = (password, confirmPass) => {
-//        const errors = []
-//        if((!password) && (!confirmPass)){
-//         errors.push('Password is required')
-//        }
 
-//    }
+   removeError = (input) => {
+       const {errors} = this.state;
+       const filteredErrors = errors.filter(el => (!el.toLowerCase().includes(input)))
+       console.log(filteredErrors);
+       this.setState({
+           errors: filteredErrors
+       })
+   }
+
 
     render(){
-        const {username, email, password, confirmPass,isSignedUp} = this.state
-            
+        const {username, email, password, confirmPass,isSignedUp,shouldCheckInput} = this.state
         return (
                 <Fade left duration={600} distance="50px">
                     <div className="form">
@@ -118,10 +185,10 @@ class SignUpForm extends React.Component{
                         (<>
                             <h1 className="form__title">Welcome!</h1>
                             <p className="form__subtitle">Please Sign Up to start making our country green!</p>
-                            <Input validation={username && this.checkInput('username')} value={username}  handleChange={this.handleChange} type="text" name={'username'} placeholder="Name..."/>
-                            <Input validation={email && this.checkInput('email')} value={email} handleChange={this.handleChange} type="email" name={'email'} placeholder="Email..."/>
-                            <Input validation={password && this.checkInput('password')} value={password} handleChange={this.handleChange} type="password" name={'password'} placeholder="Password..."/>
-                            <Input validation={confirmPass && this.checkInput('confirmPassword')} value={confirmPass} handleChange={this.handleChange} type="password" name={'confirmPass'} placeholder="Confirm Password..."/>
+                            <Input validation={shouldCheckInput && this.checkInput('username')} value={username}  handleChange={this.handleChange} type="text" name={'username'} placeholder="Name..."/>
+                            <Input validation={shouldCheckInput && this.checkInput('email')} value={email} handleChange={this.handleChange} type="email" name={'email'} placeholder="Email..."/>
+                            <Input validation={shouldCheckInput && this.checkInput('password')} value={password} handleChange={this.handleChange} type="password" name={'password'} placeholder="Password..."/>
+                            <Input validation={shouldCheckInput && this.checkInput('password')} value={confirmPass} handleChange={this.handleChange} type="password" name={'confirmPass'} placeholder="Confirm Password..."/>
                             <Input handleSubmit={this.handleSubmit} type="button" className={'form__submit'} value="Sign Up"/>
                             <Link to="/signIn" className="form__link">Already signed up?</Link>
                         </>)}
