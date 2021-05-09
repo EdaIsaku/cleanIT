0ad9ee747e1161a190d8c46385dca902643459bb
unknown <eda.isaku@yahoo.com>
2021-05-10 00:21:20 +0200

    Modified input modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,28 +1,36 @@
-
 import "./Modal.css";
+import image from "../../assets/bgImg.jpg";
 
 function Modal() {
   return (
-    <div className='modal__bg'>
-      <div className='modal'>
-        <h1 className='modal__header'>Modal Header</h1>
-        <span className='modal__close'>&#10006;</span>
-        <input className='modal__file' type='file' multiple />
+    <div className="modal__bg">
+      <div className="modal">
+        <h1 className="modal__header">Modal Header</h1>
+        <span className="modal__close">&#10006;</span>
+        <input className="modal__file" type="file" multiple />
         <textarea
-          className='modal__textarea'
-          name=''
-          id=''
-          placeholder='Please insert info about this place...'
-          cols='20'
-          rows='5'
+          className="modal__textarea"
+          name=""
+          id=""
+          placeholder="Please insert info about this place..."
+          cols="20"
+          rows="5"
         ></textarea>
-        <div className='modal__imagePreview'>
-          <img className='modal__imagePreview__1' src='' alt='' />
-          <img className='modal__imagePreview__2' src='' alt='' />
-          <img className='modal__imagePreview__3' src='' alt='' />
+        <div className="modal__imagePreview">
+          <img className="modal__imagePreview__1" src="image" alt="" />
+          <img className="modal__imagePreview__2" src="image" alt="" />
+          <img className="modal__imagePreview__3" src="image" alt="" />
         </div>
-        <input className='modal__submit' type='submit' />
-        <input className='modal__cancel' type='button' value='Cancel' />
+        <input
+          className="modal__button modal__button-submit"
+          type="submit"
+          value="Report"
+        />
+        <input
+          className="modal__button modal__button-cancel"
+          type="button"
+          value="Cancel"
+        />
       </div>
     </div>
   );
